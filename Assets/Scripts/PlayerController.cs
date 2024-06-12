73b91da90d0f8896235ddb2ac112c09dd84ee2ef
Joelcfn <elkiraque@gmail.com>
2024-06-12 14:12:06 +0100

    6.Add RigidBody components to objects 37

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,6 +14,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Move the vehicle forward
-        transform.Traslate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
