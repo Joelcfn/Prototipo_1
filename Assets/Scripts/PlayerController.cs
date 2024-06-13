c52ba01f3a2e2982f09eec07add8afc61b502345
Joelcfn <elkiraque@gmail.com>
2024-06-13 11:47:44 +0100

    4. Faça o veículo girar em vez de deslizar

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,6 +25,7 @@ public class PlayerController : MonoBehaviour
         forwardInput = Input.GetAxis("Vertical");
         // Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up, horizontalInput * turnSpeed * Time.deltaTime);
+       
     }
 }
