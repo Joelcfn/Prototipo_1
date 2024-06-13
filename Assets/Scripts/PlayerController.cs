872c0b00945fb8e57b2a7691ce16b08d8fb11b9a
Joelcfn <elkiraque@gmail.com>
2024-06-13 09:47:37 +0100

    1.Allow the vehicle to move left/right

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     //public
     public float speed = 5.0f;
+    public float turnSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -19,5 +20,6 @@ public class PlayerController : MonoBehaviour
     {
         // Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }
